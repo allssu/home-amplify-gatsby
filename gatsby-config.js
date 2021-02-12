7f54466b24beb1704ff30eb37097d06081e265b2
allssu <zxcvb6615@gmail.com>
2021-02-13 02:11:18 +0900

    ALLSSU

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `ALLSSU Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `ALLSSU`,
+      summary: `ALLSSU's amplify-gatsby blog.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://home.allssu.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `ALLSSU`,
     },
   },
   plugins: [
